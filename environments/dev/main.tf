c1ab8a50dea6088d7eb4b3467cd1503180003991
kenwoo9y <kenwoo9y@gmail.com>
2025-08-05 23:37:15 +0900

    Extract version.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,12 +1,4 @@
 terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfstatetodoapp"
